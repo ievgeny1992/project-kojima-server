674ec08c71b49e59afb7b2dbe6603b90549c6d2e
ievgeny1992 <karbash1992@gmail.com>
2022-07-27 10:39:55 +0300

    Bug fix

--- routes/games.js
+++ routes/games.js
@@ -78,26 +78,6 @@ router.get('/timeline', async (req, res) => {
   }
 });
 
-router.get('/timeline', async (req, res) => {
-  try {
-    const games = await Game.aggregate([
-      {
-        $project: {
-          year: { $year: '$addedDate' },
-          addedDate: '$addedDate',
-          name: '$name',
-          slug: '$slug',
-          coverCrop: '$coverCrop',
-        },
-      },
-      { $sort: { year: -1 } },
-    ]);
-    res.status(200).json(games);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-});
-
 //Get genres count
 router.get('/genres-count', async (req, res) => {
   let genresCount = 0;
