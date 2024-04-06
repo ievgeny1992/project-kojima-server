0ae085f3ff7a98158104a37097931b709b0c34db
ievgeny1992 <karbash1992@gmail.com>
2024-04-06 23:48:59 +0300

    Save commit

--- routes/games.js
+++ routes/games.js
@@ -38,16 +38,6 @@ router.get('/count', async (req, res) => {
   }
 });
 
-// //Get all genres
-// router.get('/genres', async (req, res) => {
-//   try {
-//     const genres = await Game.distinct('genres.name');
-//     res.status(200).json(genres);
-//   } catch (err) {
-//     res.status(500).json({ message: err.message });
-//   }
-// });
-
 //Get all genres v2
 router.get('/genres', async (req, res) => {
   try {
