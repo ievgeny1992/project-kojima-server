c68c47873c10c615eaba350d142ee5aa561d7d99
ievgeny1992 <karbash1992@gmail.com>
2025-09-24 17:42:59 +0500

    Delete deprecated property

--- app.js
+++ app.js
@@ -5,15 +5,12 @@ const cors = require('cors');
 
 const mongoose = require('mongoose');
 
-mongoose.set('useCreateIndex', true);
-mongoose.set('useUnifiedTopology', true);
-
 require('./models/Game');
 
 //Connected to DB
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-
+// mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL);
 const db = mongoose.connection;
 db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('🔗Connected to DB!'));
@@ -25,4 +22,4 @@ const router = require('./routes/games');
 app.use('/games', router);
 
 //Server started
-app.listen(3000, () => console.log("🚀Server started!"));
+app.listen(3000, () => console.log('🚀Server started!'));
